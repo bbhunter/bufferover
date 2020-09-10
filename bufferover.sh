855a3cfaa6cc4acc8ccafca224fa02d965d06439
Ahmed <32393617+ahmedcj@users.noreply.github.com>
2020-09-10 18:26:08 +0200

    v1.1

--- bufferover.sh
+++ bufferover.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-header(){ printf "\e[36m
+#v1.1
+#Made by Ahmed (Github: @ahmedcj)
+
+header(){ printf "\e[36mv1.1 @ phalanx project
 ██████╗░██╗░░░██╗███████╗███████╗███████╗██████╗░░█████╗░██╗░░░██╗███████╗██████╗░
 ██╔══██╗██║░░░██║██╔════╝██╔════╝██╔════╝██╔══██╗██╔══██╗██║░░░██║██╔════╝██╔══██╗
 ██████╦╝██║░░░██║█████╗░░█████╗░░█████╗░░██████╔╝██║░░██║╚██╗░██╔╝█████╗░░██████╔╝
 ██╔══██╗██║░░░██║██╔══╝░░██╔══╝░░██╔══╝░░██╔══██╗██║░░██║░╚████╔╝░██╔══╝░░██╔══██╗
 ██████╦╝╚██████╔╝██║░░░░░██║░░░░░███████╗██║░░██║╚█████╔╝░░╚██╔╝░░███████╗██║░░██║
-╚═════╝░░╚═════╝░╚═╝░░░░░╚═╝░░░░░╚══════╝╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝\n\e[1mtwitter: @0x0Cj\e[0m\e[0m\n\n"
-printf "Usage: ./bufferover.sh DOMAIN [OPTIONS] [-o output_file]\n\nOptions:\n\n	-d	Domains Extraction\n 	-h	Hosts Extraction\n	-s	Subdomains Extraction\n	-o	Outfile Data\n\n"
+╚═════╝░░╚═════╝░╚═╝░░░░░╚═╝░░░░░╚══════╝╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝\ntwitter: @0x0Cj\e[0m\n\n"
+printf "Usage: ./bufferover.sh DOMAIN [OPTION] [-o output_file]\n\nOptions:\n\n	-d	Domains Extraction\n 	-h	Hosts Extraction\n	-s	Subdomains Extraction\n	-sh	Subdomains Hosts Extraction\n	-o	Outfileing Data\n\n"
 }
 
 bufferover(){
@@ -20,25 +23,48 @@ else
 	if [[ "${2}" = "-d" ]]
 	then
 		LIST=$(echo "$OUTPUT" | jq '.[]' | awk -F ',' '{print $3}' | sed 's/"//' | sort -u)
-		if [[ "${3}" = "-o" ]]; then if [[ -z "${4}" ]]; then 	echo "${0}: error: please provide an output filename." >&2
-		exit 1 ; else echo "$LIST" > "${4}"; fi; else echo "$LIST"; fi
+		case ${3} in
+			-o) output "${LIST}" $4 ;;
+			 *) echo "$LIST" ;;
+		esac 
 	elif [[ "${2}" = "-h" ]]
 	then
 		LIST=$(echo "$OUTPUT" | jq '.[]' |  awk -F ',' '{print $1}' | sed 's/"//' | sort -nu)
-		if [[ "${3}" = "-o" ]]; then if [[ -z "${4}" ]]; then 	echo "${0}: error: please provide an output filename." >&2
-		exit 1 ; else echo "$LIST" > "${4}"; fi; else echo "$LIST"; fi
+		case ${3} in
+			-o) output "${LIST}" $4 ;;
+			 *) echo "$LIST" ;;
+		esac
 	elif [[ "${2}" = "-s" ]]
 	then
 		LIST=$(echo "$OUTPUT" | jq '.[]' | awk -F ',' '{print $3}' | sed 's/"//' | sort -u | grep "\.${1}$")
-		if [[ "${3}" = "-o" ]]; then if [[ -z "${4}" ]]; then 	echo "${0}: error: please provide an output filename." >&2
-		exit 1 ; else echo "$LIST" > "${4}"; fi; else echo "$LIST"; fi
+		case ${3} in
+			-o) output "${LIST}" $4 ;;
+			 *) echo "$LIST" ;;
+		esac 
+	elif [[ "${2}" = "-sh" ]]
+	then
+		LIST=$(echo "$OUTPUT" | jq '.[]' | sed 's/"//g' | grep "\.${1}$" | awk -F ',' '{print $1}' | sort -nu)
+		case ${3} in
+			-o) output "${LIST}" $4 ;;
+			 *) echo "$LIST" ;;
+		esac 
 	else
 		header
 	fi
 fi
 }
 
+output(){
+if [[ -z "${2}" ]]
+then
+	echo "${0}: error: please provide output filename." >&2
+	exit 1
+else
+	echo "${1}" > "${2}"
+fi
+}
+
 case ${2} in
-	-d|-h|-s) bufferover $1 $2 $3 $4;;
+	-d|-h|-s|-sh) bufferover $1 $2 $3 $4;;
 	*) if [[ -z "${2}" ]]; then header; else echo "${0}: error: unknown option: \"${2}\"" >&2; exit 1; fi  ;;
 esac
